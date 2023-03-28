f812ad44c90fc1bb50c66e7bdfc0ffed95b29994
oliver <oliverustc@hotmail.com>
2023-03-28 23:04:30 +0800

    learn

---
+++ lua/plugins.lua
@@ -0,0 +1,29 @@
+local packer= require('packer')
+packer.startup({
+  function(use)
+    use ('wbthomason/packer.nvim')
+    use("folke/tokyonight.nvim")
+    use({ "kyazdani42/nvim-tree.lua", requires = "kyazdani42/nvim-web-devicons" })
+    use({ "akinsho/bufferline.nvim", requires = { "kyazdani42/nvim-web-devicons", "moll/vim-bbye" }})
+    use({ "nvim-lualine/lualine.nvim", requires = { "kyazdani42/nvim-web-devicons" } })
+    use("arkav/lualine-lsp-progress")
+    use { 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } }
+  end,
+  config = {
+    display = {
+        open_fn = function()
+            return require("packer.util").float({ border = "single" })
+        end,
+    },
+  }
+})
+
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
